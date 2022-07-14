8e36f627c3b162374d0fa4e53d0837710b25adac
drfifonz <fp.patyk@gmail.com>
2022-07-14 23:48:29 +0200

    Added print of all parser's arguments and weights initialization

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+
 import argparse
 import sys
 
-from arguments_parser import arguments_parser
+from arguments_parser import arguments_parser, print_all_user_arguments
 from train import train
 from test import test
 from dotenv import load_dotenv
@@ -13,12 +14,14 @@ def main():
 
     args = arguments_parser()
 
+    print_all_user_arguments(args)
     if args.type == "test":
         train(args)
     elif args.type == "train":
         test(args)
     else:
         sys.exit("Bad type to run")
+
 
 
 if __name__ == "__main__":
