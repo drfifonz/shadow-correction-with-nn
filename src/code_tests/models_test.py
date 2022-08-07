2f4d4f415dd1bfbf2e2fd55174c469dd32fc9eb1
drfifonz <fp.patyk@gmail.com>
2022-08-08 01:34:23 +0200

    added model and trainer basic tests

---
+++ src/code_tests/models_test.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.path.insert(1, "./src")
+
+import models
+import pytest
+
+
+@pytest.fixture()
+def channels():
+    return 3, 64
+
+
+def test_deshadower(channels):
+    in_channels, out_channels = channels
+    assert models.Deshadower(in_channels, out_channels)
+
+
+def test_shadower(channels):
+    in_channels, out_channels = channels
+    assert models.Shadower(in_channels, out_channels)
+
+
+def test_discriminator(channels):
+    in_channels, _ = channels
+    assert models.Discriminator(
+        in_channels,
+    )
